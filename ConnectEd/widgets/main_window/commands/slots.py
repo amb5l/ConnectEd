136260a5b74e3256c711bbba4480976ddf425865
Adam Barnes <ambarnes@gmail.com>
2025-03-02 21:07:46 +0000

    Keyboard shortcut zoom

--- ConnectEd/widgets/main_window/commands/slots.py
+++ ConnectEd/widgets/main_window/commands/slots.py
@@ -1,6 +1,7 @@
 from PyQt6.QtWidgets import QMessageBox
 
 from ....core import logger
+from ....widgets import Drawing
 
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
@@ -12,25 +13,27 @@ class Slots:
     def __init__(self : 'Slots', parent : 'MainWindow') -> None:
         self._parent = parent
 
-    def fileExit(self):
+    def fileExit(self : 'Slots') -> None:
         self._parent.close()
 
-    def viewZoomIn(self):
-        current_widget = self._parent.mdi_area.currentSubWindow()
-        if current_widget:
-            current_widget.zoomIn()
+    def viewZoomIn(self : 'Slots') -> None:
+        current_sub_window = self._parent.mdi_area.currentSubWindow()
+        current_widget = current_sub_window.widget()
+        if isinstance(current_widget, Drawing):
+            current_widget.viewZoomIn()
 
-    def viewZoomOut(self):
-        current_widget = self._parent.mdi_area.currentSubWindow()
-        if current_widget:
-            current_widget.zoomOut()
+    def viewZoomOut(self : 'Slots') -> None:
+        current_sub_window = self._parent.mdi_area.currentSubWindow()
+        current_widget = current_sub_window.widget()
+        if isinstance(current_widget, Drawing):
+            current_widget.viewZoomOut()
 
-    def windowMessages(self):
+    def windowMessages(self : 'Slots') -> None:
         self._parent.msg_viewer.show()
 
-    def windowLog(self):
+    def windowLog(self : 'Slots') -> None:
         self._parent.log_viewer.show()
 
-    def helpAbout(self):
+    def helpAbout(self : 'Slots') -> None:
         logger.debug('helpAbout')
         QMessageBox.about(self._parent, 'About', 'ConnectEd')
