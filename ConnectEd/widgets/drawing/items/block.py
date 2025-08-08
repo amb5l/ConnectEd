ef01d9a53d4291006dfa0a41e20b9b31c319e9cc
Adam Barnes <ambarnes@gmail.com>
2025-08-08 10:26:33 +0100

    Block name/reference/path => inherent

--- ConnectEd/widgets/drawing/items/block.py
+++ ConnectEd/widgets/drawing/items/block.py
@@ -7,9 +7,9 @@ from .property_text import PropertyTextSpec
 
 class Block(PinRect):
     _PROPERTY_SPECS = PinRect._PROPERTY_SPECS | {
-        "Reference" : PropertySpec(custom=True),
-        "Name"      : PropertySpec(custom=True),
-        "Path"      : PropertySpec(custom=True)
+        "Reference" : PropertySpec(),
+        "Name"      : PropertySpec(),
+        "Path"      : PropertySpec()
     }
     _PROPERTY_TEXTS = {
         "Reference" : PropertyTextSpec( "Bottom Left" , QPointF( 0,  0 ) , "Top Left"    ),
