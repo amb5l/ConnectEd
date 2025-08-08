03907c8d43fc7c54bf559ccf90bb42d994936179
Adam Barnes <ambarnes@gmail.com>
2025-08-08 16:24:39 +0100

    QColor strings => #RRGGBB
    Deal with alpha

--- ConnectEd/core/utils.py
+++ ConnectEd/core/utils.py
@@ -66,7 +66,7 @@ def val2str(v : Any) -> str:
         case "QPointF"         : s = f"{v.x()},{v.y()}"
         case "QRectF"          : s = f"{v.x()},{v.y()},{v.width()},{v.height()}"
         case "QSizeF"          : s = f"{v.width()},{v.height()}"
-        case "QColor"          : s = hex(v.rgba())
+        case "QColor"          : s = f"#{(v.rgb() & 0xFFFFFF):06X}"
         case "PenStyle"        : s = str(v).replace("PenStyle.", "")
         case "BrushStyle"      : s = str(v).replace("BrushStyle.", "")
         case "LinePref"        : s = v.toStr()
@@ -100,7 +100,7 @@ def str2val(s : str, t : str) -> Any:
         case "QPointF"         : return QPointF(*strValuesToFloats(s))
         case "QRectF"          : return QRectF(*strValuesToFloats(s))
         case "QSizeF"          : return QSizeF(*strValuesToFloats(s))
-        case "QColor"          : return QColor.fromRgba(int(s,0))
+        case "QColor"          : return QColor.fromRgb(int(s[1:], 16) | 0xFF000000)
         case "PenStyle"        : return Qt.PenStyle[s]
         case "BrushStyle"      : return Qt.BrushStyle[s]
         case "TextPref"        : return QuillPref.fromStr(s)
