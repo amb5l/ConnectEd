f079f3831c2a697adc0af72cd513f690bad249f1
Adam Barnes <ambarnes@gmail.com>
2025-10-06 08:34:44 +0100

    Clone streamline

--- ConnectEd/widgets/graphics/items/mixin/clone.py
+++ ConnectEd/widgets/graphics/items/mixin/clone.py
@@ -11,7 +11,7 @@ ElementType = ElementMixin | ElementAnchorPointsMixin | PropertiesMixin
 
 class ElementCloneMixin:
     def clone(self : Self, original : Self | None = None) -> Self:
-        """Create a clone of this element with a new UUID."""
+        """Create a clone of this or specified element with a new UUID."""
         from ..anchor_point  import AnchorPoint
         from ..property_text import PropertyText
         from ..pin           import Pin
@@ -28,15 +28,14 @@ class ElementCloneMixin:
                 else:
                     clone_ps.value = source_ps.value
         # clone property texts and pins
-        from ..pin import Pin
         for source_child in source.childItems():
             if isinstance(source_child, Pin):
-                clone_pin = source_child.clone(source_child) # TODO is passing item needed?
+                clone_pin = source_child.clone()
                 clone_pin.setParentItem(clone)
             elif isinstance(source_child, AnchorPoint):
                 for source_ap_child in source_child.childItems():
                     if isinstance(source_ap_child, PropertyText):
-                        clone_ap_child = source_ap_child.clone(source_ap_child)
+                        clone_ap_child = source_ap_child.clone()
                         clone_ap_child.setParentItem(
                             clone._anchor_points[source_child.name()]
                         )
