cc5f84642af132a79eb5d43542339bfc24aef243
Adam Barnes <ambarnes@gmail.com>
2025-10-22 16:56:53 +0100

    Revert tree view root decoration change

--- ConnectEd/widgets/window/tree_view.py
+++ ConnectEd/widgets/window/tree_view.py
@@ -19,7 +19,6 @@ class TreeView(QTreeView):
         super().__init__(parent)
         self.setModel(model)
         self.header().setVisible(False)
-        self.setRootIsDecorated(False)
         self.setFontSize(settings().get("display/font_size"))
         self.customizeAppearance()
         self.expandAll()
