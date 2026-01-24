770adf7dc4e70157c1661ac3cc085652319b145d
Adam Barnes <ambarnes@gmail.com>
2026-01-24 22:37:08 +0000

    Tidy text appearance layout

--- ConnectEd/widgets/dialogs/components/layout/text_appearance.py
+++ ConnectEd/widgets/dialogs/components/layout/text_appearance.py
@@ -1,7 +1,7 @@
 from typing import Self
 
 from PyQt6.QtCore    import Qt
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QGridLayout, QLabel
 from PyQt6.QtGui     import QFont, QColor
 
 from ....graphics.items import Default, DEFAULT, NoChange, NO_CHANGE
@@ -25,23 +25,17 @@ class TextAppearanceLayout(QVBoxLayout):
     _default_bold      : bool   | Default
     _default_italic    : bool   | Default
     _default_underline : bool   | Default
-    _options_layout     : QVBoxLayout
-    _color_layout       : QHBoxLayout
+    _options_layout     : QGridLayout
     _color_label        : QLabel
     _color_combo        : ColorComboBox
-    _family_layout      : QHBoxLayout
     _family_label       : QLabel
     _family_combo       : FontFamilyComboBox
-    _size_layout        : QHBoxLayout
     _size_label         : QLabel
     _size_combo         : FontSizeComboBox
-    _bold_layout        : QHBoxLayout
     _bold_label         : QLabel
     _bold_combo         : OnOffComboBox
-    _italic_layout      : QHBoxLayout
     _italic_label       : QLabel
     _italic_combo       : OnOffComboBox
-    _underline_layout   : QHBoxLayout
     _underline_label    : QLabel
     _underline_combo    : OnOffComboBox
 
@@ -74,42 +68,32 @@ class TextAppearanceLayout(QVBoxLayout):
         self._default_bold      = default_bold
         self._default_italic    = default_italic
         self._default_underline = default_underline
-        self._color_layout = QHBoxLayout()
+        self._options_layout = QGridLayout()
         self._color_label = QLabel("Color:")
-        self._color_layout.addWidget(self._color_label)
+        self._options_layout.addWidget(self._color_label, 0, 0)
         self._color_combo = ColorComboBox(initial_color, default_color)
-        self._color_layout.addWidget(self._color_combo)
-        self.addLayout(self._color_layout)
-        self._family_layout = QHBoxLayout()
+        self._options_layout.addWidget(self._color_combo, 0, 1)
         self._family_label = QLabel("Family:")
-        self._family_layout.addWidget(self._family_label)
+        self._options_layout.addWidget(self._family_label, 1, 0)
         self._family_combo = FontFamilyComboBox(initial_family, default_family)
-        self._family_layout.addWidget(self._family_combo)
-        self.addLayout(self._family_layout)
-        self._size_layout = QHBoxLayout()
+        self._options_layout.addWidget(self._family_combo, 1, 1)
         self._size_label = QLabel("Size:")
-        self._size_layout.addWidget(self._size_label)
+        self._options_layout.addWidget(self._size_label, 2, 0)
         self._size_combo = FontSizeComboBox(initial_size, default_size)
-        self._size_layout.addWidget(self._size_combo)
-        self.addLayout(self._size_layout)
-        self._bold_layout = QHBoxLayout()
+        self._options_layout.addWidget(self._size_combo, 2, 1)
         self._bold_label = QLabel("Bold:")
-        self._bold_layout.addWidget(self._bold_label)
+        self._options_layout.addWidget(self._bold_label, 3, 0)
         self._bold_combo = OnOffComboBox(initial_bold, default_bold)
-        self._bold_layout.addWidget(self._bold_combo)
-        self.addLayout(self._bold_layout)
-        self._italic_layout = QHBoxLayout()
+        self._options_layout.addWidget(self._bold_combo, 3, 1)
         self._italic_label = QLabel("Italic:")
-        self._italic_layout.addWidget(self._italic_label)
+        self._options_layout.addWidget(self._italic_label, 4, 0)
         self._italic_combo = OnOffComboBox(initial_italic, default_italic)
-        self._italic_layout.addWidget(self._italic_combo)
-        self.addLayout(self._italic_layout)
-        self._underline_layout = QHBoxLayout()
+        self._options_layout.addWidget(self._italic_combo, 4, 1)
         self._underline_label = QLabel("Underline:")
-        self._underline_layout.addWidget(self._underline_label)
+        self._options_layout.addWidget(self._underline_label, 5, 0)
         self._underline_combo = OnOffComboBox(initial_underline, default_underline)
-        self._underline_layout.addWidget(self._underline_combo)
-        self.addLayout(self._underline_layout)
+        self._options_layout.addWidget(self._underline_combo, 5, 1)
+        self.addLayout(self._options_layout)
 
     def getColor(self : Self) -> QColor | Default | NoChange:
         return self._color_combo.getChoice()
