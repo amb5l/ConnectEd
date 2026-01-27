6bf6cfca8e1365f2e58ab52b113e7a8bafd79b01
Adam Barnes <ambarnes@gmail.com>
2026-01-27 16:17:10 +0000

    str() support

--- ConnectEd/core/__init__.py
+++ ConnectEd/core/__init__.py
@@ -6,9 +6,14 @@ class Text:
     value : str   # text string value
     block : bool  # False = line, True = block
 
+    def __str__(self) -> str:
+        return self.value
+
+
 @dataclass
 class TextLine(Text):
     block : bool = False
+
 
 @dataclass
 class TextBlock(Text):
