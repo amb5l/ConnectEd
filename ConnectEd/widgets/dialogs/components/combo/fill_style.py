04edcce334e9f26d888b1e7495295a435dd79d4d
Adam Barnes <ambarnes@gmail.com>
2026-02-11 16:34:38 +0000

    Fill style combo improvements

--- ConnectEd/widgets/dialogs/components/combo/fill_style.py
+++ ConnectEd/widgets/dialogs/components/combo/fill_style.py
@@ -14,26 +14,24 @@ from .. import CUSTOM_ICON_SIZE, NoChangeIcon, DefaultIcon
 
 
 class FillStyleComboBox(QComboBox):
-    STYLES = {
-        "<no change>" : NO_CHANGE,
-        "<default>"   : DEFAULT,
-        "No Fill"     : Qt.BrushStyle.NoBrush,
-        "Solid"       : Qt.BrushStyle.SolidPattern,
-        "Dense1"      : Qt.BrushStyle.Dense1Pattern,
-        "Dense2"      : Qt.BrushStyle.Dense2Pattern,
-        "Dense3"      : Qt.BrushStyle.Dense3Pattern,
-        "Dense4"      : Qt.BrushStyle.Dense4Pattern,
-        "Dense5"      : Qt.BrushStyle.Dense5Pattern,
-        "Dense6"      : Qt.BrushStyle.Dense6Pattern,
-        "Dense7"      : Qt.BrushStyle.Dense7Pattern,
-        "Hor"         : Qt.BrushStyle.HorPattern,
-        "Ver"         : Qt.BrushStyle.VerPattern,
-        "Cross"       : Qt.BrushStyle.CrossPattern,
-        "BDiag"       : Qt.BrushStyle.BDiagPattern,
-        "FDiag"       : Qt.BrushStyle.FDiagPattern,
-        "DiagCross"   : Qt.BrushStyle.DiagCrossPattern
+    _STYLES = {
+        "No Fill"   : Qt.BrushStyle.NoBrush,
+        "Solid"     : Qt.BrushStyle.SolidPattern,
+        "Dense1"    : Qt.BrushStyle.Dense1Pattern,
+        "Dense2"    : Qt.BrushStyle.Dense2Pattern,
+        "Dense3"    : Qt.BrushStyle.Dense3Pattern,
+        "Dense4"    : Qt.BrushStyle.Dense4Pattern,
+        "Dense5"    : Qt.BrushStyle.Dense5Pattern,
+        "Dense6"    : Qt.BrushStyle.Dense6Pattern,
+        "Dense7"    : Qt.BrushStyle.Dense7Pattern,
+        "Hor"       : Qt.BrushStyle.HorPattern,
+        "Ver"       : Qt.BrushStyle.VerPattern,
+        "Cross"     : Qt.BrushStyle.CrossPattern,
+        "BDiag"     : Qt.BrushStyle.BDiagPattern,
+        "FDiag"     : Qt.BrushStyle.FDiagPattern,
+        "DiagCross" : Qt.BrushStyle.DiagCrossPattern
     }
-    STYLES_REVERSE = {v: k for k, v in STYLES.items()}
+    _STYLES_REV = {v: k for k, v in _STYLES.items()}
 
     def __init__(
         self      : Self,
@@ -43,46 +41,26 @@ class FillStyleComboBox(QComboBox):
     ) -> None:
         super().__init__(parent)
         self.setIconSize(CUSTOM_ICON_SIZE)
-        if isinstance(default, Qt.BrushStyle):
-            default_icon = self.getIcon(default)
-            default_str = f" = {self.STYLES_REVERSE[default]}"
-        else:
-            default_icon = DefaultIcon().get()
-            default_str = ""
-        if isinstance(initial, Qt.BrushStyle):
-            no_change_icon = self.getIcon(initial)
-            no_change_str = f" = {self.STYLES_REVERSE[initial]}"
-        elif initial is DEFAULT:
-            no_change_icon = default_icon
-            no_change_str = f" = default{default_str}"
-        else:  # NO_CHANGE
-            no_change_icon = NoChangeIcon().get()
-            no_change_str = ""
-        for i, (k, v) in enumerate(self.STYLES.items()):
-            match k:
-                case "<no change>":
-                    icon = no_change_icon
-                    text = f"<no change{no_change_str}>"
-                case "<default>":
-                    icon = default_icon
-                    text = f"<default{default_str}>"
-                case _:
-                    icon = self.getIcon(v)
-                    text = f"{self.STYLES_REVERSE[v]}"
-            self.addItem(icon, text, v)
+        # add no change option if applicable
+        if initial is NO_CHANGE:
+            self.addItem(NoChangeIcon().get(), "<no change>", NO_CHANGE)
+            current_idx = 0
+        # add default option
+        if initial is DEFAULT:
+            current_idx = self.count()
+        default_icon = self._getIcon(default) \
+            if isinstance(default, Qt.BrushStyle) else DefaultIcon().get()
+        default_str = f" = {self._STYLES_REV[default]}" \
+            if isinstance(default, Qt.BrushStyle) else ""
+        default_value = default if isinstance(default, Qt.BrushStyle) else NO_CHANGE
+        self.addItem(default_icon, f"<default{default_str}>", default_value)
+        # add standard styles
+        for k, v in self._STYLES.items():
             if initial == v:
-                self.setCurrentIndex(i)
-
-    def getIcon(self : Self, style : Qt.BrushStyle) -> QIcon:
-        fg, bg = getFgBgColors()
-        size = self.iconSize()
-        pixmap = QPixmap(size.width(), size.height())
-        pixmap.fill(Qt.GlobalColor.transparent)
-        with QPainter(pixmap) as painter:  # Use context manager for painter
-            painter.fillRect(0, 0, size.width(), size.height(), bg)
-            painter.setBrush(QBrush(fg, style))
-            painter.drawRect(0, 0, size.width(), size.height())
-        return QIcon(pixmap)
+                current_idx = self.count()
+            self.addItem(self._getIcon(v), k, v)
+        # set current index
+        self.setCurrentIndex(current_idx)
 
     def getChoice(self : Self) -> Qt.BrushStyle | Default | NoChange:
         text = self.currentText()
@@ -91,8 +69,19 @@ class FillStyleComboBox(QComboBox):
         elif text.startswith("<default"):
             return DEFAULT
         else:
-            keys = list(self.STYLES.keys())
+            keys = list(self._STYLES.keys())
             if text in keys:
-                return self.STYLES[text]
+                return self._STYLES[text]
             logger().warning(f"Invalid fill style: {text}")
             return Qt.BrushStyle.NoBrush
+
+    def _getIcon(self : Self, style : Qt.BrushStyle) -> QIcon:
+        fg, bg = getFgBgColors()
+        size = self.iconSize()
+        pixmap = QPixmap(size.width(), size.height())
+        pixmap.fill(Qt.GlobalColor.transparent)
+        with QPainter(pixmap) as painter:
+            painter.fillRect(0, 0, size.width(), size.height(), bg)
+            painter.setBrush(QBrush(fg, style))
+            painter.drawRect(0, 0, size.width(), size.height())
+        return QIcon(pixmap)
