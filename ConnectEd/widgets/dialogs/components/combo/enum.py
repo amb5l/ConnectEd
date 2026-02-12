af8366b9351f2dd9c1b6d89f2565fb760415e276
Adam Barnes <ambarnes@gmail.com>
2026-02-12 14:09:08 +0000

    EnumComboBox improvements

--- ConnectEd/widgets/dialogs/components/combo/enum.py
+++ ConnectEd/widgets/dialogs/components/combo/enum.py
@@ -1,21 +1,27 @@
-from typing import Self
+from typing import Self, TypeVar, Generic
 from enum   import Enum
 
 from PyQt6.QtCore    import Qt
 from PyQt6.QtWidgets import QComboBox, QWidget
 
-class EnumComboBox(QComboBox):
-    _type : type[Enum]
+
+T = TypeVar("T", bound=Enum)
+
+
+class EnumComboBox(QComboBox, Generic[T]):
+    _type : type[T]
 
     def __init__(
         self    : Self,
-        initial : Enum,
+        initial : T,
         parent  : QWidget | None = None
     ) -> None:
         super().__init__(parent)
         self._type = type(initial)
         for member in self._type:
             self.addItem(member.value, member)
+            if initial == member:
+                self.setCurrentIndex(self.count() - 1)
 
-    def getChoice(self : Self) -> Enum:
+    def value(self : Self) -> Enum:
         return self.itemData(self.currentIndex(), Qt.ItemDataRole.UserRole)
