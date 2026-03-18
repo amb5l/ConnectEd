35a4f708a86ad8a83a020de414762c69a1d5b959
Adam Barnes <ambarnes@gmail.com>
2026-03-18 08:33:24 +0000

    NewPropertyDialog improvements

--- ConnectEd/widgets/dialogs/new_property.py
+++ ConnectEd/widgets/dialogs/new_property.py
@@ -11,6 +11,8 @@ from .components.combo.enum        import EnumComboBox
 
 from .components.layout.ok_cancel import OkCancelLayout
 
+from ..graphics.properties import _CUSTOM_PROPERTY_KINDS
+
 
 EditorType : TypeAlias = \
     StrEditor          | \
@@ -24,8 +26,8 @@ class NewPropertyDialog(QDialog):
     _dialog_layout    : QVBoxLayout
     _name_label       : QLabel
     _name_edit        : StrEditor
-    _kind_label       : QLabel
-    _kind_combo       : EnumComboBox[DataKind]
+    _type_label       : QLabel
+    _type_combo       : EnumComboBox[DataKind]
     _value_layout     : QHBoxLayout | QVBoxLayout
     _value_layout_idx : int
     _value_label      : QLabel
@@ -36,44 +38,47 @@ class NewPropertyDialog(QDialog):
         super().__init__(parent)
         self.setWindowTitle("New Property")
         self.setModal(True)
+        self._dialog_layout = QVBoxLayout(self)
         # name
         self._name_label = QLabel("Name:")
         self._dialog_layout.addWidget(self._name_label)
         self._name_edit = StrEditor()
         self._dialog_layout.addWidget(self._name_edit)
-        # kind
-        self._kind_label = QLabel("Kind:")
-        self._dialog_layout.addWidget(self._kind_label)
-        editors = EditorType.__args__
-        self._kind_combo = EnumComboBox[DataKind](DataKind.STR, tuple(editors))
-        self._dialog_layout.addWidget(self._kind_combo)
+        # type
+        self._type_label = QLabel("Type:")
+        self._dialog_layout.addWidget(self._type_label)
+        self._type_combo = EnumComboBox[DataKind](DataKind.STR, _CUSTOM_PROPERTY_KINDS)
+        self._dialog_layout.addWidget(self._type_combo)
         # value
         self._value_layout_idx = self._dialog_layout.count()
+        self._value_label = QLabel("Value:")
         self._onKindChanged()
+        # ok/cancel
+        self._ok_cancel_layout = OkCancelLayout(self)
+        self._dialog_layout.addLayout(self._ok_cancel_layout)
         # finalise
-        self._kind_combo.currentIndexChanged.connect(self._onKindChanged)
-        self._onKindChanged()
+        self._type_combo.currentIndexChanged.connect(self._onKindChanged)
 
-    def _onKindChanged(self : Self, index : int) -> None:
+    def _onKindChanged(self : Self, _index : int = 0) -> None:
         """Change editor widget based on selected kind."""
         # remove current layout if it exists
         if hasattr(self, '_value_layout'):
             self._value_layout.deleteLater()
             self._dialog_layout.removeItem(self._value_layout)
         # create new widget based on selected kind
-        kind = self._kind_combo.value()
-        editor = kind.editor
-        self._value_layout = QVBoxLayout() if kind == DataKind.TEXT else QHBoxLayout()
-        self._dialog_layout.insertLayout(self._value_layout_idx, self._value_layout)
-        self._value_label = QLabel("Value:")
+        kind = self._type_combo.value()
+        self._value_layout = QVBoxLayout() \
+            if kind == DataKind.TEXT else QHBoxLayout()
         self._value_layout.addWidget(self._value_label)
-        self._value_layout.addWidget(editor)
+        self._value_edit = kind.editor()()
+        self._value_layout.addWidget(self._value_edit)
+        self._dialog_layout.insertLayout(self._value_layout_idx, self._value_layout)
 
     def getName(self : Self) -> str:
         return self._name_edit.text()
 
     def getKind(self : Self) -> DataKind:
-        return self._kind_combo.value()
+        return self._type_combo.value()
 
     def getValue(self : Self) -> Any:
         return self._value_edit.value()
