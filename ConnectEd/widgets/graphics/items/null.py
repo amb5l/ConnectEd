2d7772babfa2dc7996d7846be941c815dab08075
Adam Barnes <ambarnes@gmail.com>
2026-03-25 17:40:04 +0000

    NullItem: streamline

--- ConnectEd/widgets/graphics/items/null.py
+++ ConnectEd/widgets/graphics/items/null.py
@@ -2,13 +2,9 @@ from typing import Self
 
 from PyQt6.QtCore    import QRectF, QXmlStreamWriter
 from PyQt6.QtWidgets import QGraphicsItem, QStyleOptionGraphicsItem, QWidget
-from PyQt6.QtGui     import QPainter, QPainterPath, QMouseEvent
+from PyQt6.QtGui     import QPainter, QPainterPath
 
 from ....app import logger
-
-from typing import TYPE_CHECKING
-if TYPE_CHECKING:
-    from . import ItemMixin
 
 
 class NullItem(QGraphicsItem):
@@ -16,7 +12,7 @@ class NullItem(QGraphicsItem):
     _brect  : QRectF        # bounding rect
     _hshape : QPainterPath  # shape for hit detection
 
-    def __init__(self : Self, parent : "ItemMixin | None" = None) -> None:
+    def __init__(self : Self, parent : QGraphicsItem | None = None) -> None:
         super().__init__(parent)
         self.setFlag( self.GraphicsItemFlag.ItemHasNoContents , True  )
         self.setFlag( self.GraphicsItemFlag.ItemIsMovable     , False )
@@ -25,22 +21,6 @@ class NullItem(QGraphicsItem):
         self._brect = QRectF()
         self._hshape = QPainterPath()
         self._hshape.addRect(self._brect)
-
-    def mousePressEvent(self : Self, event : QMouseEvent):
-        event.ignore()
-        return
-
-    def mouseMoveEvent(self : Self, event : QMouseEvent):
-        event.ignore()
-        return
-
-    def mouseReleaseEvent(self : Self, event : QMouseEvent):
-        event.ignore()
-        return
-
-    def mouseDoubleClickEvent(self : Self, event : QMouseEvent):
-        event.ignore()
-        return
 
     def boundingRect(self : Self) -> QRectF:
         return self._brect
@@ -57,5 +37,5 @@ class NullItem(QGraphicsItem):
         logger().error("Paint should never be called")
 
     def toXml(self : Self, xw : QXmlStreamWriter) -> None:
-        """Dummy toXml method - NullPoints are not serialized."""
+        """Dummy toXml method - NullItem is not serialized."""
         pass
