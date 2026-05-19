ab64f23472cbba9c744139190e134d540e15fb5e
Adam Barnes <ambarnes@gmail.com>
2026-05-19 22:37:52 +0100

    Fix sentinels

--- ConnectEd/core/types.py
+++ ConnectEd/core/types.py
@@ -10,16 +10,45 @@ from PyQt6.QtGui     import QColor
 class Default:
     def __eq__(self : Self, other : object) -> bool:
         return isinstance(other, Default)
-    def __hash__(self : Self) -> int: return hash(Default)
-    def __str__(self : Self) -> str: return "default"
-    def __repr__(self : Self) -> str: return "<default>"
+
+    def __hash__(self : Self) -> int:
+        return hash(Default)
+
+    def __copy__(self : Self) -> "Default":
+        return DEFAULT
+
+    def __deepcopy__(self : Self, _memo : object) -> "Default":
+        return DEFAULT
+
+    def __str__(self : Self) -> str:
+        return "default"
+
+    def __repr__(self : Self) -> str:
+        return "<default>"
+
 
 DEFAULT = Default()
 
 
 class NoChange:
-    def __str__(self : Self): return "no change"
-    def __repr__(self : Self): return "<no change>"
+    def __eq__(self : Self, other : object) -> bool:
+        return isinstance(other, NoChange)
+
+    def __hash__(self : Self) -> int:
+        return hash(NoChange)
+
+    def __copy__(self : Self) -> "NoChange":
+        return NO_CHANGE
+
+    def __deepcopy__(self : Self, _memo : object) -> "NoChange":
+        return NO_CHANGE
+
+    def __str__(self : Self) -> str:
+        return "no change"
+
+    def __repr__(self : Self) -> str:
+        return "<no change>"
+
 
 NO_CHANGE = NoChange()
 
